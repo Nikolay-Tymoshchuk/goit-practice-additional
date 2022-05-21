c9da4128402c8d8f45087e0434755ab5e0fa395f
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-05-22 01:44:54 +0300

    Quokka add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const arr = [];
+import menuData from './js/menu.json'
+import menueTpl from './templates/list-render.hbs'
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const test = document.querySelector('#theme-switch-toggle');
+console.log('object :>> ', test);
+
+test.addEventListener('change', onChange);
+function onChange(e) {
+	
+}
