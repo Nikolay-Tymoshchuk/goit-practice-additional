4693cdd1c1a1fe93593197d488f40438579d840a
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-05-22 13:36:38 +0300

    fix impotr hbs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import menuData from './js/menu.json'
-import menueTpl from './templates/list-render.hbs'
+import menuData from './js/menu.json';
+import menueTpl from './templates/list-render.hbs';
+
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+const refs = {
+  menu: document.querySelector('.js-menu'),
+  switcher: document.getElementById('theme-switch-toggle')
+}
+
+refs.switcher.addEventListener('change', changeTheme);
+function changeTheme(e) {
+  e.target.checked ? document.body.classList.add(Theme.DARK) : document.body.classList.remove(Theme.DARK)
+}
+
+refs.menu.insertAdjacentHTML('beforeend', menueTpl(menuData));
